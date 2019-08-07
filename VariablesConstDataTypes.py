a5f711505c91374dbc5d365cfe4235f86ffe1fff
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-08-07 18:12:47 +0530

    Variables constants and data types

---
+++ VariablesConstDataTypes.py
@@ -0,0 +1,33 @@
+import constants
+
+var = 10
+
+int_var = 20
+
+print("Value of int_var:",int_var)
+print("Type of int_var:",type(int_var))
+
+float_var = 20.34
+
+print("Value of float_var:",float_var)
+print("Type of float_var:",type(float_var))
+
+str_var = "Codekul"
+
+print("Value of str_var:",str_var)
+print("Type of str_var:",type(str_var))
+
+bool_var = True
+print("Value of bool_var:",bool_var)
+print("Type of bool_var:",type(bool_var))
+
+int_var = "This is now str"
+print("Value of int_var:",int_var)
+print("Type of int_var:",type(int_var))
+
+print(var)
+
+constants.PI = 3.146
+
+print(constants.PI)
+print(constants.BATCH_NAME)
---
+++ constants.py
@@ -0,0 +1,2 @@
+PI = 3.14
+BATCH_NAME = "Python-Aug-2019-Weekday"
