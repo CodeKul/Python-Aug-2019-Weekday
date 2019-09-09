77e69c377a430309fbb31037d6f1e0e0a0ca453b
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-09-09 18:21:24 +0530

    polymorphism

---
+++ PolyMorphism.py
@@ -0,0 +1,62 @@
+class Employee:
+    def __init__(self, empid, name, salary):
+        self.empid = empid
+        self.name = name
+        self.salary = salary
+    
+    def display(self):
+        print('Id: ', self.empid)
+        print('Name: ', self.name)
+        print('Salary: ', self.salary)
+
+
+class Developer(Employee):
+    def __init__(self, empid, name, salary, deskId, raise_sal):
+        self.deskId = deskId
+        self.raise_sal = raise_sal
+        Employee.__init__(self, empid, name, salary)
+
+    def display(self):
+        Employee.display(self)
+        print('Desk ID: ', self.deskId)
+
+    def increamnet(self):
+        self.salary = self.salary * self.raise_sal
+
+
+class Manager(Employee):
+    def __init__(self, empid, name, salary, cabinId, raise_sal):
+        self.cabinId = cabinId
+        self.raise_sal = raise_sal
+        Employee.__init__(self, empid, name, salary)
+
+    def display(self):
+        Employee.display(self)
+        print('Cabin ID: ', self.cabinId)
+
+    def increamnet(self):
+        self.salary = self.salary * self.raise_sal
+
+class Company:
+    def __init__(self, employees):
+        self.employees = employees
+
+    def show_all_employees(self):
+        for emp in self.employees:
+            emp.display()
+
+    def raise_salary(self, emp):
+        emp.increamnet()
+
+d1 = Developer(1, 'ABC', 5000, 101, 1.3)
+m1 = Manager(2, 'XYZ', 8000, 201, 1.5)
+
+# d1.display()
+# m1.display()
+
+list1 = [d1, m1]
+com = Company(list1)
+com.show_all_employees()
+
+com.raise_salary(com.employees[1])
+com.show_all_employees()
