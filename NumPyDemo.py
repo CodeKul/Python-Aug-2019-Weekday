71be26f01623772249ee26efb8a58f23af93164b
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-10-23 18:38:06 +0530

    matplot lib tuts

--- NumPyDemo.py
+++ NumPyDemo.py
@@ -60,3 +60,30 @@ print(a - b)
 print(a * b)
 
 print(a.dot(b))
+
+
+arr = np.array([[1, 5, 6], [4, 7, 2], [3, 1, 9]]) 
+print(arr.shape)
+print(arr.max(axis=0))
+print(arr.min(axis=1))
+
+a = np.array([0, np.pi/2, np.pi]) 
+print(a)
+print ("Sine values of array elements:", np.sin(a)) 
+
+
+a = np.array([0, 1, 2, 3]) 
+print ("Exponent of array elements:", np.exp(a)) 
+
+print ("Square root of array elements:", np.sqrt(a)) 
+
+
+# Sorting array:
+
+a = np.array([[1, 4, 2], [3, 4, 6], [0, -1, 5]]) 
+  
+# sorted array 
+print ("Array elements in sorted order:\n", np.sort(a, axis = None)) 
+  
+# sort array row-wise 
+print ("Row-wise sorted array:\n", -np.sort(-a, axis = 1)) 
